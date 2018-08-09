9dd6438a7f4c021200cdc014f71ed1f3a024edeb
Simon Sun <9262981@qq.com>
2018-08-09 23:07:01 +0800

    no message

---
+++ app/src/main/java/com/coding25/activitytest/FirstActivity.java
@@ -0,0 +1,25 @@
+package com.coding25.activitytest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class FirstActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.first_layout);
+        Button button1=(Button) findViewById(R.id.button);
+        button1.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Toast.makeText(FirstActivity.this,"You clicked button",
+                        Toast.LENGTH_LONG).show();
+
+            }
+        });
+    }
+}
